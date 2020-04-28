f327aa47dded9a9e39215aa1d45461961bf7356d
kimbeomgyu <qjarb1994@gmail.com>
2020-04-28 23:15:16 +0900

    student Struct 2

--- main.go
+++ main.go
@@ -21,6 +21,17 @@ func (s Student) ViewSungjuk() {
 	fmt.Println(s.sungjuk)
 }
 
+// InputSungjuk is 성적을 입력한다
+func (s Student) InputSungjuk(name string, grade string) {
+	s.sungjuk.name = name
+	s.sungjuk.grade = grade
+}
+
+// ViewSungjuk is 성적을 출력한다
+func ViewSungjuk(s Student) {
+	fmt.Println(s.sungjuk)
+}
+
 func main() {
 	var s Student
 
@@ -30,5 +41,15 @@ func main() {
 	s.sungjuk.name = "수학"
 	s.sungjuk.grade = "C"
 
-	s.ViewSungjuk()
+	// 같은 기능이다
+	s.ViewSungjuk() // 수학 C
+	ViewSungjuk(s)  // 수학 C
+
+	// 과학 A 로 변경되지 않는다 입력이 되었을때
+	// 입력되는 값과 함수 내부에서 사용되는 값은 완전히 다른 메모리 주소값이다.
+	// 복사로 동작해서 기능이 동작하지 않는다.
+	// 이걸 해결하기 위해 포인터를 이용한다.
+	s.InputSungjuk("과학", "A")
+
+	s.ViewSungjuk() // 수학 C
 }
