ad2afb868d6feef28c069b249f37d3cce7ae5490
kimbeomgyu <qjarb1994@gmail.com>
2020-05-02 06:29:57 +0900

    InputNumbers

--- numberBaseball/main.go
+++ numberBaseball/main.go
@@ -66,6 +66,57 @@ func MakeNumbers() [3]int {
 // InputNumbers is 키보드로부터 0~9사이의 겹치지 않는 숫자 3개를 입력을 받아 반환한다
 func InputNumbers() [3]int {
 	var result [3]int
+
+	for {
+		fmt.Println("겹치지 않는 0 ~ 9 사이의 숫자 3개를 입력해 주세요")
+		var no int
+		_, err := fmt.Scanf("%d\n", &no)
+		if err != nil {
+			fmt.Println("잘못 입력하였습니다")
+			continue
+		}
+
+		success := true
+		idx := 0
+		for no > 0 {
+			n := no % 10
+			no = no / 10
+
+			duplicated := false
+			for j := 0; j < idx; j++ {
+				if result[j] == n {
+					// 겹치는 경우 다시 뽑는다.
+					duplicated = true
+					break
+				}
+			}
+			if duplicated {
+				fmt.Println("숫자가 겹치지 않아야 합니다")
+				success = false
+				break
+			}
+			if idx >= 3 {
+				fmt.Println("3개보다 많은 숫자를 입력하셧습니다")
+				success = false
+				break
+			}
+
+			result[idx] = n
+			idx++
+		}
+		if success && idx < 3 {
+			fmt.Println("3개의 숫자를 입력하세요")
+			success = false
+		}
+
+		if !success {
+			continue
+		}
+
+		break
+	}
+	result[0], result[2] = result[2], result[0] // reverse
+	fmt.Println(result)
 	return result
 }
 
