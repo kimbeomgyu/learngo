2794d255d4e8a95eb4729a96af3b6b2a1080ff92
kimbeomgyu <qjarb1994@gmail.com>
2020-05-01 20:57:45 +0900

    pointer

--- main.go
+++ main.go
@@ -4,52 +4,30 @@ import "fmt"
 
 // Student is 학생이다
 type Student struct {
-	name  string
-	class int
+	name string
+	age  int
 
-	sungjuk Sungjuk
-}
-
-// Sungjuk is 학생의 성적이다
-type Sungjuk struct {
-	name  string
+	class string
 	grade string
 }
 
 // ViewSungjuk is 성적을 출력한다
-func (s Student) ViewSungjuk() {
-	fmt.Println(s.sungjuk)
+func (s *Student) ViewSungjuk() {
+	fmt.Println(s.class, s.grade)
 }
 
 // InputSungjuk is 성적을 입력한다
-func (s Student) InputSungjuk(name string, grade string) {
-	s.sungjuk.name = name
-	s.sungjuk.grade = grade
+func (s *Student) InputSungjuk(class string, grade string) {
+	s.class = class
+	s.grade = grade
 }
 
-// ViewSungjuk is 성적을 출력한다
-func ViewSungjuk(s Student) {
-	fmt.Println(s.sungjuk)
-}
+// 포인터를 함수 인자로 받으면 메모리 주소만 복사
+// 값을 함수 인자로 받으면 전체 속성을 복사
 
 func main() {
-	var s Student
+	var s Student = Student{name: "Beomgyu", age: 23, class: "수학", grade: "A+"}
 
-	s.name = "Smith"
-	s.class = 1
-
-	s.sungjuk.name = "수학"
-	s.sungjuk.grade = "C"
-
-	// 같은 기능이다
-	s.ViewSungjuk() // 수학 C
-	ViewSungjuk(s)  // 수학 C
-
-	// 과학 A 로 변경되지 않는다 입력이 되었을때
-	// 입력되는 값과 함수 내부에서 사용되는 값은 완전히 다른 메모리 주소값이다.
-	// 복사로 동작해서 기능이 동작하지 않는다.
-	// 이걸 해결하기 위해 포인터를 이용한다.
-	s.InputSungjuk("과학", "A")
-
-	s.ViewSungjuk() // 수학 C
+	s.InputSungjuk("과학", "C")
+	s.ViewSungjuk()
 }
