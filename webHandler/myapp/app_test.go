c7ae9d1d83332a29ae7b2162bc706ee713b50dd9
kimbeomgyu <qjarb1994@gmail.com>
2020-05-15 09:58:47 +0900

    web handler testing 2

--- webHandler/myapp/app_test.go
+++ webHandler/myapp/app_test.go
@@ -1,9 +1,11 @@
 package myapp
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -24,7 +26,6 @@ func TestIndexPathHandler(t *testing.T) {
 
 	data, _ := ioutil.ReadAll(res.Body)
 	assert.Equal("Hello World", string(data))
-
 }
 
 func TestBarPathHandler_WithoutName(t *testing.T) {
@@ -40,7 +41,6 @@ func TestBarPathHandler_WithoutName(t *testing.T) {
 
 	data, _ := ioutil.ReadAll(res.Body)
 	assert.Equal("Hello World!!", string(data))
-
 }
 
 func TestBarPathHandler_WithName(t *testing.T) {
@@ -56,5 +56,35 @@ func TestBarPathHandler_WithName(t *testing.T) {
 
 	data, _ := ioutil.ReadAll(res.Body)
 	assert.Equal("Hello tucker!!", string(data))
+}
+
+func TestFooPathHandler_WithoutJson(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/foo", nil)
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusBadRequest, res.Code)
+}
+
+func TestFooPathHandler_WithJson(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("POST", "/foo", strings.NewReader(`{"first_name":"tucker", "last_name":"kim", "email":"tucker@naver.com"}`))
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusCreated, res.Code)
+
+	user := new(User)
+	err := json.NewDecoder(res.Body).Decode(user)
+	assert.Nil(err)
+	assert.Equal("tucker", user.FirstName)
+	assert.Equal("kim", user.LastName)
 
 }
