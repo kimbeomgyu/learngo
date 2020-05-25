6d69c3e1e3d81b878ed5d9f4f8bca4cbb77afa90
kimbeomgyu <qjarb1994@gmail.com>
2020-05-25 11:43:58 +0900

    todo app setting

--- TodoApp/public/todo.js
+++ TodoApp/public/todo.js
@@ -18,6 +18,28 @@
       }
     });
 
+    var addItem = function (item) {
+      if (item.completed) {
+        todoListItem.append(
+          "<li class='completed'><div class='form-check'><label class='form-check-label'><input class='checkbox' checked='checked' type='checkbox' />" +
+            item.name +
+            "<i class='input-helper'></i></label></div><i class='remove mdi mdi-close-circle-outline'></i></li>"
+        );
+      } else {
+        todoListItem.append(
+          "<li><div class='form-check'><label class='form-check-label'><input class='checkbox' type='checkbox' />" +
+            item.name +
+            "<i class='input-helper'></i></label></div><i class='remove mdi mdi-close-circle-outline'></i></li>"
+        );
+      }
+    };
+
+    $.get("/todos", function (items) {
+      items.forEach((e) => {
+        addItem(e);
+      });
+    });
+
     todoListItem.on("change", ".checkbox", function () {
       if ($(this).attr("checked")) {
         $(this).removeAttr("checked");
