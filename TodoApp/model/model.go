d089c2265a70326af0160d701f842eed68b210c0
kimbeomgyu <qjarb1994@gmail.com>
2020-05-27 08:22:58 +0900

    Todo App Interface

--- TodoApp/model/model.go
+++ TodoApp/model/model.go
@@ -1,8 +1,6 @@
 package model
 
-import (
-	"time"
-)
+import "time"
 
 // Todo is Todo
 type Todo struct {
@@ -12,43 +10,35 @@ type Todo struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
-var todoMap map[int]*Todo
+type dbHandler interface {
+	getTodos() []*Todo
+	addTodo(name string) *Todo
+	removeTodo(id int) bool
+	completeTodo(id int, complete bool) bool
+}
+
+var handler dbHandler
 
 func init() {
-	todoMap = make(map[int]*Todo)
+	handler = newMemoryHandler()
 }
 
 // GetTodos is get todo list
 func GetTodos() []*Todo {
-	list := []*Todo{}
-	for _, v := range todoMap {
-		list = append(list, v)
-	}
-	return list
+	return handler.getTodos()
 }
 
 // AddTodo is add todo item
 func AddTodo(name string) *Todo {
-	id := len(todoMap) + 1
-	todo := &Todo{id, name, false, time.Now()}
-	todoMap[id] = todo
-	return todo
+	return handler.addTodo(name)
 }
 
 // RemoveTodo is remove todo item
 func RemoveTodo(id int) bool {
-	if _, ok := todoMap[id]; ok {
-		delete(todoMap, id)
-		return true
-	}
-	return false
+	return handler.removeTodo(id)
 }
 
 // CompleteTodo is complete todo item
 func CompleteTodo(id int, complete bool) bool {
-	if todo, ok := todoMap[id]; ok {
-		todo.Completed = complete
-		return true
-	}
-	return false
+	return handler.completeTodo(id, complete)
 }
