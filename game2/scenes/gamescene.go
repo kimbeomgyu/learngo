8c0d44e5883c65d2c981aab8c0c55d5368cfb2fb
kimbeomgyu <qjarb1994@gmail.com>
2020-06-06 15:12:03 +0900

    Game Puzzle part 2

--- game2/scenes/gamescene.go
+++ game2/scenes/gamescene.go
@@ -3,7 +3,9 @@ package scenes
 import (
 	"image"
 	"learngo/game2/global"
+	"learngo/game2/scenemanager"
 	"log"
+	"math/rand"
 
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
@@ -18,8 +20,11 @@ type GameScene struct {
 	blankX, blankY int
 }
 
+var gameoverBoard [global.PuzzleColumns][global.PuzzleRows]int
+
 // Startup is GameScene Startup
 func (g *GameScene) Startup() {
+
 	var err error
 	g.bgimg, _, err = ebitenutil.NewImageFromFile("./images/monalisa.png", ebiten.FilterDefault)
 	if err != nil {
@@ -31,8 +36,19 @@ func (g *GameScene) Startup() {
 
 	for i := 0; i < global.PuzzleColumns; i++ {
 		for j := 0; j < global.PuzzleRows; j++ {
-
 			g.subImages[j*global.PuzzleColumns+i] = g.bgimg.SubImage(image.Rect(i*width, j*height, i*width+width, j*height+height)).(*ebiten.Image)
+		}
+	}
+
+	arr := make([]int, global.PuzzleColumns*global.PuzzleRows-1)
+	idx := 0
+	for i := 0; i < global.PuzzleColumns; i++ {
+		for j := 0; j < global.PuzzleRows; j++ {
+			if i == global.PuzzleColumns-1 && j == global.PuzzleRows-1 {
+				continue
+			}
+			arr[j*global.PuzzleColumns+i] = idx
+			idx++
 		}
 	}
 
@@ -43,16 +59,34 @@ func (g *GameScene) Startup() {
 		for j := 0; j < global.PuzzleRows; j++ {
 			if i == g.blankX && j == g.blankY {
 				g.board[i][j] = -1
-			} else {
-				g.board[i][j] = j*global.PuzzleColumns + i
+				continue
+			}
+			idx := rand.Intn(len(arr))
+			g.board[i][j] = arr[idx]
+			arr = append(arr[:idx], arr[idx+1:]...)
+		}
+	}
 
+	for i := 0; i < global.PuzzleColumns; i++ {
+		for j := 0; j < global.PuzzleRows; j++ {
+			if i == g.blankX && j == g.blankY {
+				gameoverBoard[i][j] = -1
+			} else {
+				gameoverBoard[i][j] = j*global.PuzzleColumns + i
 			}
 		}
 	}
+
 }
 
 // Update is GameScene update
 func (g *GameScene) Update(screen *ebiten.Image) error {
+
+	if g.board == gameoverBoard {
+		scenemanager.SetScene(&StartScene{})
+		return nil
+	}
+
 	if inpututil.IsKeyJustReleased(ebiten.KeyUp) {
 		if g.blankY > 0 {
 			g.board[g.blankX][g.blankY] = g.board[g.blankX][g.blankY-1]
